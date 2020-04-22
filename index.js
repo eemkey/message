c2ff4ae99ff3d8f7fe817687808e55e6a5233850
Emily Ma <em.kayama@gmail.com>
2020-04-22 10:16:56 -0400

    add ability to view secret message

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 
+const messageForm = document.querySelector("#message-form");
+const linkForm = document.querySelector("#link-form");
+const messageShow = document.querySelector("#message-show")
+
+const {hash} = window.location;
+const message = atob(hash.replace("#", ""));
+if (message) {
+    messageForm.classList.add("hide");
+    messageShow.classList.remove("hide");
+    document.querySelector("h1").innerHTML = message;
+}
+
 document.querySelector("form").addEventListener("submit", e => {
     e.preventDefault();
-    document.querySelector("#message-form").classList.add("hide");
-    document.querySelector("#link-form").classList.remove("hide");
+    messageForm.classList.add("hide");
+    linkForm.classList.remove("hide");
 const input = document.querySelector("#message-input");
 const encrypted = btoa(input.value);
 const linkInput = document.querySelector("#link-input")
